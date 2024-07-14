f7d134910e200254aa106d9bd652c8004d3e13b4
joycewangui <joycewangui590@gmail.com>
2024-07-14 14:24:18 +0700

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+const mainElement = document.getElementById('main');
+if (mainElement) {
+  mainElement.remove();
+}
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+newHeader.textContent = 'NANA is the champion';
